ba08cc925e9aee93ce3cc1461ec9037ed53dda60
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-03-22 16:15:19 -0300

    feat: Organize Program.cs

---
+++ src/web/EMS.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,13 @@
+﻿using EMS.WebApp.MVC.Data;
+
+namespace EMS.WebApp.MVC.Configuration;
+
+public static class DependencyInjectionConfig
+{
+    public static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+    {
+        //services.AddSingleton<IValidationAttributeAdapterProvider, CpfValidationAttributeAdapterProvider>();
+        services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
+        services.AddScoped<EMSDbContext>();
+    }
+}
--- src/web/EMS.WebApp.MVC/Program.cs
+++ src/web/EMS.WebApp.MVC/Program.cs
@@ -1,51 +1,14 @@
-using EMS.WebApp.MVC.Data;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Authentication.Cookies;
+using EMS.WebApp.MVC.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddControllersWithViews();
-
-builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => 
-    options.SignIn.RequireConfirmedAccount = false
-).AddEntityFrameworkStores<ApplicationDbContext>();
-
-//builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
-//                .AddCookie(options =>
-//                {
-//                    options.LoginPath = "/Identity/Account/Login";
-//                    options.AccessDeniedPath = "/erro/403";
-//                });
-builder.Services.AddDbContext<EMSDbContext>(options =>
-    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"))
-);
-
-builder.Services.AddScoped<EMSDbContext>();
+builder.Services.AddMvcConfiguration(builder.Configuration);
+builder.Services.RegisterServices(builder.Configuration);
 
 var app = builder.Build();
 
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    app.UseHsts();
-}
+app.UseMvcConfiguration(app.Environment, app);
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
-app.UseRouting();
-
-app.UseAuthentication();
-app.UseAuthorization();
-
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+app.CheckAndApplyDatabaseMigrations(app.Services);
 
 app.Run();
