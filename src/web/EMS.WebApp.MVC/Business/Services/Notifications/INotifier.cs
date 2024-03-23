fbeecb28865114b5bdc32c2af3909379b7fb2673
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-03-23 14:28:31 -0300

    feat: Implement notifications between layers

---
+++ src/web/EMS.WebApp.MVC/Business/Services/Notifications/INotifier.cs
@@ -0,0 +1,8 @@
+﻿namespace EMS.WebApp.MVC.Business.Services.Notifications;
+
+public interface INotifier
+{
+    bool HasNotification();
+    List<Notification> GetNotifications();
+    void Handle(Notification notification);
+}
--- src/web/EMS.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
+++ src/web/EMS.WebApp.MVC/Configuration/DependencyInjectionConfig.cs
@@ -1,3 +1,4 @@
+using EMS.WebApp.MVC.Business.Services.Notifications;
 ﻿using EMS.WebApp.MVC.Business.Utils.User;
 using EMS.WebApp.MVC.Data;
 using EMS.WebApp.MVC.Extensions;
@@ -13,6 +14,7 @@ public static class DependencyInjectionConfig
         
         services.AddSingleton<IHttpContextAccessor, HttpContextAccessor>();
         services.AddScoped<IAspNetUser, AspNetUser>();
+        services.AddScoped<INotifier, Notifier>();
         
         services.AddScoped<EMSDbContext>();
     }
