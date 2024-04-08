682825a368998444e0cbe5a2c51f679cf940e20f
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-04-07 21:49:05 -0300

    feat: Add Tenant Repository

---
+++ src/web/EMS.WebApp.MVC/Business/Interfaces/Repository/ITenantRepository.cs
@@ -0,0 +1,12 @@
+﻿using EMS.WebApp.MVC.Business.Models;
+
+namespace EMS.WebApp.MVC.Business.Interfaces.Repository;
+
+public interface ITenantRepository : IRepository<Tenant>
+{
+    Task<Tenant> AddTenant();
+    Task<bool> DeleteTenant(Guid id);
+    Task<IEnumerable<Tenant>> GetAllTenants();
+    Task<Tenant> GetTenantById(Guid id);
+    Task<bool> Block(Guid id);
+}
