344f8d139c79029cd5a2de321fc2f14ac1fb8640
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-04-19 23:42:52 -0300

    feat: Add Notification View Component

---
+++ src/web/EMS.WebApp.MVC/Extensions/NotificationViewComponent.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace EMS.WebApp.MVC.Extensions;
+
+public class NotificationViewComponent : ViewComponent
+{
+    public IViewComponentResult Invoke()
+    {
+        var successMessage = TempData["Success"] as string;
+        var failureMessage = TempData["Failure"] as string;
+
+        if (!string.IsNullOrEmpty(successMessage))
+        {
+            ViewData["MessageType"] = "success";
+            ViewData["Message"] = successMessage;
+        }
+        else if (!string.IsNullOrEmpty(failureMessage))
+        {
+            ViewData["MessageType"] = "danger";
+            ViewData["Message"] = failureMessage;
+        }
+
+        return View();
+    }
+}
