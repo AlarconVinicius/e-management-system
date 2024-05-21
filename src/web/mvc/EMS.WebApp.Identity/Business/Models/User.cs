248ab900673f9b88ea2ee3771b7cdcbb4bc72514
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-05-21 15:16:35 -0300

    feat: Add Identity Project

---
+++ src/web/mvc/EMS.WebApp.Identity/Business/Models/User.cs
@@ -0,0 +1,129 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace EMS.WebApp.Identity.Business.Models;
+
+public class User
+{
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    [DisplayName("E-mail")]
+    public string Email { get; set; }
+
+    public User() { }
+
+    public User(Guid id, string email)
+    {
+        Id = id;
+        Email = email;
+    }
+}
+
+public class RegisterUser
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    public Guid Id { get; set; }
+
+    [DisplayName("Cargo")]
+    public string Role { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    [DisplayName("E-mail")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    [DisplayName("Senha")]
+    public string Password { get; set; } = string.Empty;
+}
+
+public class LoginUser
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido.")]
+    public string Email { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório.")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres.", MinimumLength = 6)]
+    [DisplayName("Senha")]
+    public string Password { get; set; } = string.Empty;
+}
+
+public class UpdateUser
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [DisplayName("Id")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres.", MinimumLength = 2)]
+    [DisplayName("Nome")]
+    public string Name { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres.", MinimumLength = 2)]
+    [DisplayName("Sobrenome")]
+    public string LastName { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [EmailAddress(ErrorMessage = "O campo {0} está em formato inválido")]
+    [DisplayName("E-mail")]
+    public string Email { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [DisplayName("Celular")]
+    public string PhoneNumber { get; set; }
+
+    [DisplayName("CPF")]
+    public string Cpf { get; set; }
+
+    [DisplayName("Ativo")]
+    public bool IsActive { get; set; }
+
+    public UpdateUserPassword UpdateUserPasswordViewModel { get; set; }
+    public UpdateUser() { }
+    public UpdateUser(Guid id, string name, string lastName, string email, string phoneNumber, string cpf)
+    {
+        Id = id;
+        Name = name;
+        LastName = lastName;
+        Email = email;
+        PhoneNumber = phoneNumber;
+        Cpf = cpf;
+    }
+}
+
+public class UpdateUserPassword
+{
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [DisplayName("Id")]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    [DisplayName("Senha Atual")]
+    public string OldPassword { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 6)]
+    [DisplayName("Senha")]
+    public string Password { get; set; } = string.Empty;
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [Compare("Password", ErrorMessage = "As senhas não conferem.")]
+    [DisplayName("Confirme sua senha")]
+    public string ConfirmPassword { get; set; } = string.Empty;
+
+    public UpdateUserPassword() { }
+
+    public UpdateUserPassword(Guid id, string oldPassword, string password, string confirmPassword)
+    {
+        Id = id;
+        OldPassword = oldPassword;
+        Password = password;
+        ConfirmPassword = confirmPassword;
+    }
+}
