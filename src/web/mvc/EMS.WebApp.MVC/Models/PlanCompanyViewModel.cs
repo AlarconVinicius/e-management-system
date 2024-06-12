dbd81b84ce18f77c49b4d0b176d822f2b19f0335
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-06-12 20:58:25 -0300

    feat: Starting to use the API

--- src/web/mvc/EMS.WebApp.MVC/Models/PlanCompanyViewModel.cs
+++ src/web/mvc/EMS.WebApp.MVC/Models/PlanCompanyViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace EMS.WebApp.MVC.Models;
+﻿using EMS.Core.Requests.Identities;
+using EMS.Core.Responses.Plans;
+
+namespace EMS.WebApp.MVC.Models;
 
 public class PlanCompanyViewModel
 {
-    public PlanViewModel Plan { get; set; } = new PlanViewModel();
+    public PlanResponse Plan { get; set; } = new PlanResponse();
+    public CreateUserRequest CreateUserRequest { get; set; } = new CreateUserRequest();
     public RegisterCompanyViewModel RegisterCompany { get; set; } = new RegisterCompanyViewModel();
 }
