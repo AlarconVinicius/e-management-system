d60e620ed2b6fa703afb07f7bdab1f276066d9ea
Vinícius Alarcon <alarcon.pqd74@gmail.com>
2024-06-20 22:42:19 -0300

    feat: Add DateTime regex

--- src/building blocks/EMS.Core/Utils/RegexUtils.cs
+++ src/building blocks/EMS.Core/Utils/RegexUtils.cs
@@ -1,5 +1,10 @@
 ﻿namespace EMS.Core.Utils;
+
 public class RegexUtils
 {
     public const string TimeSpanPattern = @"^(?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)$";
+
+    public const string DateTimeWithHourPattern = @"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4} (?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)$";
+
+    public const string DateTimeWithoutHourPattern = @"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4}$";
 }
