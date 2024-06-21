9241f2ea6bc2bbf5d825e1aea38679c49985823d
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-06-21 14:28:26 -0300

    feat: Improve TimeSpan Regex

--- src/building blocks/EMS.Core/Utils/RegexUtils.cs
+++ src/building blocks/EMS.Core/Utils/RegexUtils.cs
@@ -2,7 +2,9 @@
 
 public class RegexUtils
 {
-    public const string TimeSpanPattern = @"^(?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)$";
+    public const string TimeSpanWithHourPattern = @"^(?:[01]\d|2[0-3]):(?:[0-5]\d):(?:[0-5]\d)$";
+
+    public const string TimeSpanWithoutHourPattern = @"^(?:[01]\d|2[0-3]):(?:[0-5]\d)$";
 
     public const string DateTimeWithHourPattern = @"^(0[1-9]|[12][0-9]|3[01])/(0[1-9]|1[0-2])/\d{4} (?:[01]\d|2[0123]):(?:[012345]\d):(?:[012345]\d)$";
 
