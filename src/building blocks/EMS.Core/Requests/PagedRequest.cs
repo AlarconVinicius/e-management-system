a1118e51935893d7b803f08fddf75aece45d60a2
AlarconVinicius <alarcon.pqd74@gmail.com>
2024-07-02 07:46:46 -0300

    feat: Add property to PagedRequest

--- src/building blocks/EMS.Core/Requests/PagedRequest.cs
+++ src/building blocks/EMS.Core/Requests/PagedRequest.cs
@@ -7,13 +7,15 @@ public abstract class PagedRequest : Request
     public int PageSize { get; set; } = ConfigurationDefault.DefaultPageSize;
     public int PageNumber { get; set; } = ConfigurationDefault.DefaultPageNumber;
     public string Query { get; set; } = null;
+    public bool ReturnAll { get; set; }
 
     public PagedRequest() { }
 
-    protected PagedRequest(int pageSize = ConfigurationDefault.DefaultPageSize, int pageNumber = ConfigurationDefault.DefaultPageNumber, string query = null)
+    protected PagedRequest(int pageSize = ConfigurationDefault.DefaultPageSize, int pageNumber = ConfigurationDefault.DefaultPageNumber, string query = null, bool returnAll = false)
     {
         PageSize = pageSize;
         PageNumber = pageNumber;
         Query = query;
+        ReturnAll = returnAll;
     }
 }
